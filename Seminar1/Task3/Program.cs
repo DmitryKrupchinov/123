b8ab8f4b693a5e9a5539a408f31c7a983bcedc62
Dmitry Krupchinov <krupchinovd@mail.ru>
2024-05-21 03:24:14 +0300

    1

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает сумму
+// первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int firstDigit = num / 100;
+int lustDigit = num % 10;
+
+if (num >= 100 && num <= 999)
+{
+    int result = firstDigit + lustDigit;
+    Console.Write($"Сумма первой и последней цифры = {result}"); //Console.Write("Сумма первой и последней цифры = " + result);
+}
+else
+{
+    Console.Write("неправильный ввод");
+}
